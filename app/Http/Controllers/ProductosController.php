3049f7a3e91b3e4a05a70beaa48e00bbb054f928
gerardoadame <gera_skate@outlook.com>
2020-08-21 17:51:25 -0500

    producto y categorias terminado

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -14,7 +14,7 @@ class ProductosController extends Controller
         
         $pto=DB::table('productos')
         ->where('productos.id','=',$val)
-        ->select('productos.producto','productos.precio','productos.detalles')
+        ->select('productos.producto','productos.precio','productos.detalles','productos.ruta_img')
         ->get(); 
         // dd($val);
     	return view('productos',compact('pto'));
